218376e2566ae9e6d1c087b494d60abcb14b0d71
Baras64 <47410182+Baras64@users.noreply.github.com>
2019-08-03 22:28:35 +0530

    Add files via upload

--- CubeItUp2.java
+++ CubeItUp2.java
@@ -1,13 +1,13 @@
 public class CubeItUp2 {
     public static void main(String args[]){
-        System.out.println(computeCube(5)*computeCube(500)*computeCube(5000000000L)%1000007 );
+        System.out.println((computeCube(5)*computeCube(500)*computeCube(5000000000L))%1000007 );
     }
     public static long computeCube(long n){
         long multiples=n/9;
         long sum=multiples*54;
         long remaining=n%9;
-        for(long i=0;i<remaining;i++){
-            sum+=computeSum((long)Math.pow((n-i),3));
+        for(long i=1;i<=remaining;i++){
+            sum+=computeSum((long)Math.pow((i),3));
         }
         return sum%1000007;
     }
