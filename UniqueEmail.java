e8619dd0c83d85a863bea9a09892b43cacb204e3
Baras64 <47410182+Baras64@users.noreply.github.com>
2019-08-03 17:22:42 +0530

    Add files via upload

--- UniqueEmail.java
+++ UniqueEmail.java
@@ -1,6 +1,8 @@
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
+
 public class UniqueEmail {
     public static void main(String args[]){
         Scanner input=new Scanner(System.in);
@@ -10,33 +12,29 @@ public class UniqueEmail {
         System.out.println("Number of unique emails is= "+n);
     }
     public static int countEmail(String[] arr) {
-        Set<String> set = new HashSet<>();
-
-        for(String email : arr){
-            StringBuilder stringBuilder = new StringBuilder();
-            for(int i = 0 ; i < email.length(); i++){
-                char ch = email.charAt(i);
-                if(ch == '+'){
-                    while(email.charAt(i) != '@'){
-                        i++;
-                    }
-                    stringBuilder.append(email.substring(i));
-                    break;
-                }
-                else if(ch == '.'){
-                    continue;
-                }
-                else if(ch == '@') {
-                    stringBuilder.append(email.substring(i));
-                    break;
-                }
-                else{
-                    stringBuilder.append(ch);
+        HashSet<String> count=new HashSet<>();
+        String temp="";
+        for(String email:arr){
+            String firstPart=email.substring(0,email.indexOf('@'));
+            String secondPart=email.substring(email.indexOf('@'));
+            if(firstPart.contains("+")){
+                firstPart=firstPart.substring(0,firstPart.indexOf("+"));
+            }
+            for(int i=0;i<firstPart.length();i++){
+                char ch=firstPart.charAt(i);
+                if(ch!='.'){
+                    temp+=ch;
                 }
             }
-            set.add(stringBuilder.toString());
+            firstPart=temp;
+            count.add(firstPart+secondPart);
+            temp="";
         }
-        return set.size();
+        Iterator<String> itr=count.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        return count.size();
     }
 }
 
