9626fcebb2dcc50e6567b213afe425b979652238
Baras64 <47410182+Baras64@users.noreply.github.com>
2019-08-03 16:08:14 +0530

    Add files via upload

--- UniqueEmail.java
+++ UniqueEmail.java
@@ -1,7 +1,6 @@
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-
 public class UniqueEmail {
     public static void main(String args[]){
         Scanner input=new Scanner(System.in);
@@ -10,19 +9,34 @@ public class UniqueEmail {
         int n=countEmail(arr);
         System.out.println("Number of unique emails is= "+n);
     }
-    public static int countEmail(String arr[]){
-        Set<String> count=new HashSet<>();
-        for(String iterator:arr) {
-            int index=iterator.indexOf('@');
-            String firstPart=iterator.substring(0,index);
-            String secondPart=iterator.substring(index);
-            if(firstPart.contains("+")){
-                int plusIndex=firstPart.indexOf('+');
-                firstPart=firstPart.substring(0,plusIndex);
+    public static int countEmail(String[] arr) {
+        Set<String> set = new HashSet<>();
+
+        for(String email : arr){
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i = 0 ; i < email.length(); i++){
+                char ch = email.charAt(i);
+                if(ch == '+'){
+                    while(email.charAt(i) != '@'){
+                        i++;
+                    }
+                    stringBuilder.append(email.substring(i));
+                    break;
+                }
+                else if(ch == '.'){
+                    continue;
+                }
+                else if(ch == '@') {
+                    stringBuilder.append(email.substring(i));
+                    break;
+                }
+                else{
+                    stringBuilder.append(ch);
+                }
             }
-            firstPart.replaceAll(".","");
-            count.add(firstPart+secondPart);
+            set.add(stringBuilder.toString());
         }
-        return count.size();
+        return set.size();
     }
 }
+
