9e740e2aa662f7e532af9a77fea9e01d2a10411f
Baras64 <47410182+Baras64@users.noreply.github.com>
2019-08-10 19:06:20 +0530

    Update MinimumItem.java

--- MinimumItem.java
+++ MinimumItem.java
@@ -6,17 +6,17 @@ public class MinimumItem{
 
     public static int countItem(int amount)
     {
-        int itemAmount[] ={ 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 };
+        int itemAmount[] ={2048,1024,512,256,128,64,32,16,8,4,2,1};
         int items[]= new int[12];
 
-        for (int i = 0; i < 12; i++) {
+        for (int i=0; i<12; i++) {
             if (amount >= itemAmount[i]) {
-                items[i] = amount / itemAmount[i];
-                amount = amount - items[i] * itemAmount[i];
+                items[i]=amount/itemAmount[i];
+                amount=amount-items[i]*itemAmount[i];
             }
         }
         int k=0;
-        for (int i = 0; i < 12; i++) {
+        for (int i=0; i<12; i++) {
             if (items[i] != 0) {
                 k+=items[i];
             }
