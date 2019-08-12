05d5c4fcf27f2dcea282afe0273e6a0fd8eb107a
Baras64 <47410182+Baras64@users.noreply.github.com>
2019-08-12 21:46:55 +0530

    Update Marble.java

--- Marble.java
+++ Marble.java
@@ -1,9 +1,12 @@
 public class Marble {
     public static void main(String args[]){
         System.out.println(computeWinner(22)+computeWinner(45)+computeWinner(26)+computeWinner(75));
+
     }
-    public static int computeWinner(int n){
-        int winner[]={2,1,1,2,2,2,1,1,1};
-        return winner[n%8];
+    public static int computeWinner(int n) {
+        if(n%3==0)
+            return 2;
+        else
+            return 1;
     }
 }
